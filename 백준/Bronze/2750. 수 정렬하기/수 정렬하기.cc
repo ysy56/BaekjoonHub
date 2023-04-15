5cca57b2536804f669a53b2cf3529195d453bc44
moony <78634780+ysy56@users.noreply.github.com>
2023-04-15 16:09:36 +0900

    [Bronze II] Title: 수 정렬하기, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+
+  cin >> N;
+  int arr[N];
+
+  for(int i = 0; i<N; i++){
+    cin >> arr[i];
+  }
+  
+  for(int i = 0; i<N; i++){
+    for(int j = i+1; j<N; j++){
+      if(arr[i] > arr[j]){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
+
+  for(int i = 0; i<N; i++){
+    cout << arr[i] << endl;
+  }
+  
+  return 0;
+}
