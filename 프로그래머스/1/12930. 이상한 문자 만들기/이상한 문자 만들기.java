4f061f0781eea620e362c99041bb3bd8c31fe4f6
moony <78634780+ysy56@users.noreply.github.com>
2024-04-04 22:56:52 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.14 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -30,14 +30,14 @@ class Solution {
         for (int i = 0; i < len; i++) {
             if (s.charAt(i) == ' '){
                 idx = 0;
-                sb.append((char)s.charAt(i));
+                sb.append(s.charAt(i));
             }
             else if (idx % 2 == 0 && s.charAt(i) >= 'a' && s.charAt(i) <= 'z')
                 sb.append((char)(s.charAt(i) - 32));
             else if (idx % 2 != 0 && s.charAt(i) >= 'A' && s.charAt(i) <= 'Z')
                 sb.append((char)(s.charAt(i) + 32));
             else
-                sb.append((char)s.charAt(i));
+                sb.append(s.charAt(i));
             if ((s.charAt(i) >= 'a' && s.charAt(i) <= 'z') 
                 || (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z'))
                 idx++;
