00847a0f64f88910e72dde50e164336b6c9c2c7b
moony <78634780+ysy56@users.noreply.github.com>
2024-05-24 09:38:46 +0900

    [level 1] Title: K번째수, Time: 0.34 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,37 +1,14 @@
-// input : 임의의 숫자 배열(int array)
-        // [i, j, k]를 원소로 가지는 2차원 배열(int[][] commands)
-// variable : 리턴해야할 배열의 크기(int size)
-            // 리턴해야할 배열(int[] answer)
-
-/* process
-
-commands의 크기 구하기
-answer의 크기 할당 및 선언
-commands의 크기만큼 반복문(row = 0)
-    i, j, k값 넣어주기
-    (i-1) ~ (j-1)까지의 배열 복사
-    복사된 배열 정렬 후 (k-1)의 요소 answer[row]에 넣기
-*/
-
-// return : array에서 i부터 j까지 자르고 정렬한 후 k번째 숫자들의 배열
-
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int size = commands.length;
-        int[] answer = new int[size];
+        int[] answer = new int[commands.length];
         
-        for (int row = 0; row < size; row++) {
-            int i = commands[row][0];
-            int j = commands[row][1];
-            int k = commands[row][2];
-            
-            int[] temp = Arrays.copyOfRange(array, i-1, j);
+        for (int i = 0; i<commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
             Arrays.sort(temp);
-            answer[row] = temp[k-1];
+            answer[i] = temp[commands[i][2] - 1];
         }
-        
         return answer;
     }
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 77.1 MB, 시간: 0.55 ms
+메모리: 78.6 MB, 시간: 0.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 15:22:52
+2024년 05월 24일 09:38:42
 
 ### 문제 설명
 
