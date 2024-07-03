0e2548b369b457a8385a4c4b55a236675da2df3f
moony <78634780+ysy56@users.noreply.github.com>
2024-07-04 08:13:51 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,28 +1,13 @@
-// input : 빈병(int a), 새로운 콜라(int b), 내가 가진 빈병(int n)
-// variable : 새로 받은 콜라(int answer)
-            // 남는 병(int temp)
-
-/* process
-내가 가진 빈병이 빈병보다 크거나 같으면 반복
-    남는 병 = 내가 가진 빈병 % 빈병
-    새로 받은 콜라 += (내가 가진 빈병 / 빈병) * 새로운 콜라
-    내가 가진 빈병 = (내가 가진 빈병 / 빈병) * 새로운 콜라 + 남는 병
-
-*/
-
-// result : 내가 가진 빈병으로 교환했을 때 새로운 콜라병 수
-
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        int temp = 0;
         
         while (n >= a) {
-            temp = n % a;
-            answer += (n / a) * b;
-            n = (n / a) * b + temp;
+            int submitCola = n - (n % a);
+            int addCola = n / a * b;
+            n = n - submitCola + addCola;
+            answer += addCola;
         }
-        
         return answer;
     }
 }
