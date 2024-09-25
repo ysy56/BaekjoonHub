ce4af9611358bcd951b1eda523c988e460080fd0
moony <78634780+ysy56@users.noreply.github.com>
2024-09-25 12:20:39 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,8 +1,10 @@
 class Solution {
     public int solution(int num1, int num2) {
         int answer = -1;
+        
         if (num1 == num2)
             answer = 1;
+        
         return answer;
     }
 }
