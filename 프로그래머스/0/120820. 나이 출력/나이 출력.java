f5c4e49cefc5df0676a128ac6732ee29548efddf
moony <78634780+ysy56@users.noreply.github.com>
2024-09-25 12:21:58 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/0/120820. 나이 출력/나이 출력.java
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int age) {
-        int answer = 0;
-        answer = 2022 - (age - 1);
+        int answer = 2022 - age + 1;
         return answer;
     }
 }
