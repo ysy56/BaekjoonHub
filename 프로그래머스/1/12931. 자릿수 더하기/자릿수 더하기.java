9884a87279cc57d92b19adf2a7f055d6c08f8878
moony <78634780+ysy56@users.noreply.github.com>
2024-09-27 11:27:39 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 64.9 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,8 +4,8 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        while(n > 0) {
-            answer += n % 10;
+        while (n > 0) {
+            answer += n%10;
             n /= 10;
         }
 
