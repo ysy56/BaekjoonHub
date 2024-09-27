d1db2214735f57611f581f696d8465cd37bc2a30
moony <78634780+ysy56@users.noreply.github.com>
2024-09-27 11:31:09 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.04 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -0,0 +1,19 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = false;
+        int p = 0;
+        int y = 0;
+        
+        for (int i = 0; i<s.length(); i++) {
+            if (s.charAt(i) == 'p' || s.charAt(i) == 'P')
+                p++;
+            else if (s.charAt(i) == 'y' || s.charAt(i) == 'Y')
+                y++;
+        }
+        
+        if (p == y)
+            answer = true;
+
+        return answer;
+    }
+}
