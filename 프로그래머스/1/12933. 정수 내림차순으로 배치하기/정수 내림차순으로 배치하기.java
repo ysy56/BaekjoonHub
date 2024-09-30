cb58952bc3999ae3e3a6136d86e25b0b8608aa50
moony <78634780+ysy56@users.noreply.github.com>
2024-09-30 22:04:41 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.05 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,21 +1,31 @@
-import java.util.Arrays;
-
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        int len = Long.toString(n).length();
-        long[] numArr = new long[len];
-        String[] strArr = Long.toString(n).split("");
+        int size = String.valueOf(n).length();
+        long[] tmpArr = new long[size];
         
-        for (int i = 0; i<len; i++) {
-            numArr[i] = Long.parseLong(strArr[i]);
+        for (int i = 0; i<size; i++) {
+            tmpArr[i] = n%10;
+            n /= 10;
         }
-        Arrays.sort(numArr);
-        answer = numArr[len-1];
-        for (int i = len-2; i>=0; i--) {
+        
+        for (int i = 0; i<size; i++) {
+            for (int j = i+1; j<size; j++) {
+                if (tmpArr[i] < tmpArr[j]) {
+                    long tmp = tmpArr[i];
+                    tmpArr[i] = tmpArr[j];
+                    tmpArr[j] = tmp;
+                }
+            }
+        }
+        
+        for (int i = 0; i<size; i++) {
+            answer += tmpArr[i];
+            if (i == size - 1)
+                break;
             answer *= 10;
-            answer += numArr[i];
         }
+        
         return answer;
     }
 }
