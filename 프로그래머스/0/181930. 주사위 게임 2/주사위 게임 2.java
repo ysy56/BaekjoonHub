69322550e971b765d9416a9fc8de53bfc35e9143
moony <78634780+ysy56@users.noreply.github.com>
2024-10-07 22:12:26 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        if ((a != b) && (b != c) && (c != a))
+            return a + b + c;
+        else if ((a == b) && (b == c) && (c == a))
+            return (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+        else
+            return (a + b + c) * (a*a + b*b + c*c);
+    }
+}
