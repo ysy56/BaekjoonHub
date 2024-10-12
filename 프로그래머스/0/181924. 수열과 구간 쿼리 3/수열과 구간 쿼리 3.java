dc0a554395f6bac11bc0f9b42f4194be47b277dc
moony <78634780+ysy56@users.noreply.github.com>
2024-10-12 14:50:07 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.08 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = arr;
+        
+        for (int i = 0; i<queries.length; i++) {
+            int temp = arr[queries[i][0]];
+            arr[queries[i][0]] = arr[queries[i][1]];
+            arr[queries[i][1]] = temp;
+        }
+        
+        return answer;
+    }
+}
