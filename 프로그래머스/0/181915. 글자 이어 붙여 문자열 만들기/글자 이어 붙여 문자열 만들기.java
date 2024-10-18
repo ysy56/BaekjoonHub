818f0c3d8c77c4365f80d9799171645327e9827b
moony <78634780+ysy56@users.noreply.github.com>
2024-10-18 23:19:20 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.19 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int[] index_list) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i<index_list.length; i++) {
+            sb.append(my_string.charAt(index_list[i]));
+        }
+        
+        String answer = sb.toString();
+        
+        return answer;
+    }
+}
