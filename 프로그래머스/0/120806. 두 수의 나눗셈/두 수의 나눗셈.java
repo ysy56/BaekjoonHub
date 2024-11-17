86766fabbb9f0c4d1a0c20769249ef4c87453e7e
moony <78634780+ysy56@users.noreply.github.com>
2024-11-17 22:16:42 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        answer = (int)((double)num1/num2 * 1000);
-        return answer;
+        double answer = (double)num1 / num2 * 1000; 
+        return (int)answer;
     }
 }
