1dc5b17e107a0b5cfc37d8f1730730b32c547fdf
moony <78634780+ysy56@users.noreply.github.com>
2024-11-17 22:26:15 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = my_string.length() - 1; i>=0; i--) {
+            sb.append(my_string.charAt(i));
+        }
+        
+        String answer = sb.toString();
+        
+        return answer;
+    }
+}
