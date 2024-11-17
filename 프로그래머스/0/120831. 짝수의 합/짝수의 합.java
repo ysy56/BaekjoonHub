92cf4dc64192c69116516cbd3f887d97ca9490e1
moony <78634780+ysy56@users.noreply.github.com>
2024-11-17 22:18:16 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,8 +1,12 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i = 0; i <= n; i += 2)
-            answer += i;
+        
+        for (int i = 0; i<=n; i++) {
+            if (i%2 == 0)
+                answer += i;
+        }
+        
         return answer;
     }
 }
