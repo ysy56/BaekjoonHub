c35c099b28907d0a6b48313223d0baf13bcb6a76
moony <78634780+ysy56@users.noreply.github.com>
2025-02-16 14:19:37 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.25 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i<my_string.length(); i++) {
+            if(my_string.charAt(i) == ' '){
+                list.add(sb.toString());
+                sb.setLength(0);
+            }
+            else {
+                sb.append(my_string.charAt(i));
+            }
+        }
+        list.add(sb.toString());
+        
+        String[] answer = list.toArray(new String[list.size()]);
+        
+        return answer;
+    }
+}
