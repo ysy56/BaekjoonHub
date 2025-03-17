00007e01df678ce13171d1c234d63fb94a021699
moony <78634780+ysy56@users.noreply.github.com>
2025-03-17 11:08:04 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 1.44 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length - 5];
+        
+        Arrays.sort(num_list);
+        
+        for (int i = 0; i<num_list.length - 5; i++) {
+            answer[i] = num_list[5+i];    
+        }
+        
+        return answer;
+    }
+}
