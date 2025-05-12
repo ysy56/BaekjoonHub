8420d4c4a3fc2ce61212686484c9a670914f1107
moony <78634780+ysy56@users.noreply.github.com>
2025-05-12 20:20:48 +0900

    [level 0] Title: l로 만들기, Time: 10.10 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i<myString.length(); i++) {
+            if (myString.charAt(i) < 'l')
+                sb.append("l");
+            else
+                sb.append(myString.charAt(i));
+        }
+        
+        String answer = sb.toString();
+        
+        return answer;
+    }
+}
